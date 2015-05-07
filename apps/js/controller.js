303c8560c96665944503460e14e011994e02fd92
Jemo Mgebrishvili <jemo.mgebrishvili@gmail.com>
2015-05-07 21:52:11 +0400

    changed structure

--- apps/js/controller.js
+++ apps/js/controller.js
@@ -1,8 +1,31 @@
 angular.module('myApp.controllers',[]).
-controller('FinanceController',function($scope){
-	$scope.salary = 0;
-	$scope.percentage = 0;
-	$scope.result = function() {
-		return $scope.salary * $scope.percentage * 0.01;
-	};
+controller('CurrencyController',function($scope){
+	
+	$scope.USD = 0;
+	$scope.GEL = 0;
+	$scope.usd_to_euro = 0;
+	$scope.euro_to_gel = 0;
+	$scope.gel_to_euro = 0;
+	$scope.euro_to_usd = 0;
+
+
+	$scope.USD_to_GEL = function() {
+		return $scope.USD * 2.3258;
+	}
+	$scope.GEL_to_USD = function() {
+		return $scope.GEL / 2.3258;
+	} 
+	$scope.USD_to_EURO = function() {
+		return $scope.usd_to_euro * 2.6128;
+	}
+	$scope.EURO_to_USD = function() {
+		return $scope.euro_to_usd / 2.3258;
+	}
+	$scope.EURO_to_GEL = function() {
+		return $scope.euro_to_gel * 2.6128;
+	}
+	$scope.GEL_to_EURO = function() {
+		return $scope.gel_to_euro / 2.6128;
+	}
+
 });
